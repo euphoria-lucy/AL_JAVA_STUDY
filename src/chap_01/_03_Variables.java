e4181d98d6a755ac732d94ff5f22443a56d4025d
misung <voielactee7@naver.com>
2023-05-21 16:31:26 +0900

    IntelliJ_변수(float, long)

--- src/chap_01/_03_Variables.java
+++ src/chap_01/_03_Variables.java
@@ -16,5 +16,15 @@ public class _03_Variables {
         boolean pass = true;
 
         System.out.println("합격여부 : " + pass);
+        
+        double d = 65.37;
+        float f = 65.37F; // float는 숫자뒤에 F를 붙여주어야 함
+
+        System.out.println(d);
+        System.out.println(f);
+
+        long l = 1000000000000L; // long은 숫자 뒤에 L을 붙어주어야함
+
+        System.out.println(l);
     }
 }
