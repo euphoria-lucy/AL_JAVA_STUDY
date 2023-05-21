265ecddbb7cb8c6504c0a205059fcf685e24fce3
misung <voielactee7@naver.com>
2023-05-21 16:38:49 +0900

    IntelliJ_주석

---
+++ src/chap_01/_04_Comment.java
@@ -0,0 +1,17 @@
+package chap_01;
+
+public class _04_Comment {
+    public static void main(String[] args) {
+
+        // 한 줄 주석 단축키 : Ctrl + /
+        // 여러 줄 주석 단축키 : Ctrl + shift + /
+
+        System.out.println("(10분 전) 잠시 후 ssg landers와 한화 이글스의 시즌 1차전 경기가 시작됩니다.");
+        // System.out.println("(5분 전) 잠시 후 ssg landers와 한화 이글스의 시즌 1차전 경기가 시작됩니다.");
+        System.out.println("지금부터 ssg landers와 한화 이글스의 시즌 1차전 경기를 시작하겠습니다.");
+
+        int a = 65;
+        int b = 37;
+        System.out.println(a + b);
+    }
+}
