ea5c9470fe015f016f2ea2950b826e8e90ace3a1
misung <voielactee7@naver.com>
2023-05-21 16:00:27 +0900

    IntelliJ_자료형

---
+++ src/chap_01/_02_DataType.java
@@ -0,0 +1,13 @@
+package chap_01;
+
+public class _02_DataType {
+    public static void main(String[] args) {
+        System.out.println("JAVA"); // 문자열
+        System.out.println("IntelliJ");
+        System.out.println(65); // 정수
+        System.out.println(-51);
+        System.out.println(4.37); // 실수
+        System.out.println(true); // boolean
+        System.out.println(false); // boolean
+    }
+}
