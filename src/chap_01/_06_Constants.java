fa89eccdb0e3f1556f8da964d9b756d8b2402ae4
misung <voielactee7@naver.com>
2023-05-25 09:05:22 +0900

    IntelliJ_상수

---
+++ src/chap_01/_06_Constants.java
@@ -0,0 +1,14 @@
+package chap_01;
+
+public class _06_Constants {
+    public static void main(String[] args) {
+
+        // 상수로 변수선언할 때는 변수명이 대문자여야함!!
+        final String KR_COUNTRY_CODE = "+82"; // 국가번호
+        System.out.println(KR_COUNTRY_CODE);
+
+        final double PI = 3.141592; // 원주율
+        final String DATE_OF_BIRTH = "2004 - 03 - 07"; // 생년월일
+
+    }
+}
