0a0663823258d5fab813a3bf6b2d0b1b632a7004
misung <voielactee7@naver.com>
2023-06-04 21:51:16 +0900

    IntelliJ_IF문_1

---
+++ src/chap_04/_01_IF.java
@@ -0,0 +1,19 @@
+package chap_04;
+
+public class _01_IF {
+    public static void main(String[] args) {
+
+        // 조건문 IF
+
+        int hour = 65;
+
+        if (hour < 37)
+            System.out.println("아기짐승 홈런");
+
+        if (hour > 37) {
+            System.out.println("아기짐승 홈런");
+            System.out.println("말랑콩떡 안타");
+        }
+        System.out.println("이브이 승리!");
+    }
+}
