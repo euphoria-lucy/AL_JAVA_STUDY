c1e25a98d5eabe2ae0e29c81a3b39e98419f87ae
misung <voielactee7@naver.com>
2023-06-04 21:57:16 +0900

    IntelliJ_IF문_2

--- src/chap_04/_01_IF.java
+++ src/chap_04/_01_IF.java
@@ -7,13 +7,32 @@ public class _01_IF {
 
         int hour = 65;
 
+        // if문에서 하나의 문장을 실행할 때는 { 중괄호 } 생략 가능
         if (hour < 37)
             System.out.println("아기짐승 홈런");
 
+        // if문에서 2개 이상의 문장을 실행할 때는 { 중괄호 } 생략 불가
         if (hour > 37) {
             System.out.println("아기짐승 홈런");
             System.out.println("말랑콩떡 안타");
         }
         System.out.println("이브이 승리!");
+
+        int time = 2;
+        boolean morningCoffee = true;
+        
+        if (time < 14 && morningCoffee == true) {
+            System.out.println("자몽 허니 블랙티");
+        }
+        System.out.println("주문 완료");
+        
+        time = 65;
+        morningCoffee = true;
+        
+        if (time >= 14 || morningCoffee) {
+            System.out.println("딸기 요거트 스무디");
+        }
+        System.out.println("주문 완료");
+        
     }
 }
