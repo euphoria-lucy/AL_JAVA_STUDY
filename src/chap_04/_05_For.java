98f03220a5884864b3906754a069db71ef4ce398
misung <voielactee7@naver.com>
2023-06-07 17:25:31 +0900

    [JAVA] IntelliJ_반복문 For_1

---
+++ src/chap_04/_05_For.java
@@ -0,0 +1,14 @@
+package chap_04;
+
+public class _05_For {
+    public static void main(String[] args) {
+        // 반복문 For
+
+        // 반복문 사용 For
+        // for문의 형식 : for (선언; 조건; 증감) {}
+        for ( int i = 0; i < 10; i++ ) {
+            System.out.println("아기짐승 홈런");
+        }
+
+    }
+}
