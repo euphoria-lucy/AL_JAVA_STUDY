78fe0528e9c4cd3acd0778cf870bedc8cd3257f9
MISUNG <113699085+euphoria-lucy@users.noreply.github.com>
2023-07-15 13:52:21 +0900

    [JAVA] IntelliJ_다차원 배열 순회

---
+++ src/chap_05/_04_MultiArrayLoop.java
@@ -0,0 +1,65 @@
+package chap_05;
+
+public class _04_MultiArrayLoop {
+    public static void main(String[] args) {
+
+        // 다차원 배열 순회
+
+        String[][] seats = new String[][] {
+                { "A1", "A2", "A3", "A4", "A5" },
+                { "B1", "B2", "B3", "B4", "B5" },
+                { "C1", "C2", "C3", "C4", "C5" }
+        };
+
+        for (int i = 0; i < 3; i++) { // 세로
+            for (int j = 0; j < 5; j++) { // 가로
+                System.out.print(seats[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("-----------------------");
+        System.out.println();
+
+        String[][] seats_t = {
+                { "A1", "A2", "A3" },
+                { "B1", "B2", "B3", "B4" },
+                { "C1", "C2", "C3", "C4", "C5" }
+        };
+
+        for (int i = 0; i < seats_t.length; i++) { // 세로
+            for (int j = 0; j < seats_t[i].length; j++) { // 가로
+                System.out.print(seats_t[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("-----------------------");
+        System.out.println();
+
+        // 세로 크기 10 X 가로 크기 15에 해당하는 영화관 좌석
+        String[][] seats_thr = new String[10][15];
+        String[] eng =  { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J" };
+
+        for (int i = 0; i < seats_thr.length; i++) {
+            for (int j = 0; j < seats_thr[i].length; j++) {
+                seats_thr[i][j] = eng[i] + (j + 1);
+            }
+        }
+
+        // 표 구매
+        seats_thr[7][8] = "--"; // H9
+        seats_thr[7][9] = "--"; // H10
+
+        // 영화관 좌석 번호 확인
+        for (int i = 0; i < seats_thr.length; i++) { // 세로
+            for (int j = 0; j < seats_thr[i].length; j++) { // 가로
+                System.out.print(seats_thr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
