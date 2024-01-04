deba7f0105a6d79def70b1c1a0c417c5ff832d29
misung <voielactee7@naver.com>
2024-01-04 18:57:23 +0900

    [JAVA] AnonymousClass_2

---
+++ src/chap_10/_02_AnonymousClass_2.java
@@ -0,0 +1,37 @@
+package chap_10;
+
+public class _02_AnonymousClass_2 {
+    public static void main(String[] args) {
+        HomeMadeBurger momMadeBurger = getMomMadeBurger();
+        momMadeBurger.cook();
+
+        System.out.println("---------------------------------------------------");
+
+        HomeMadeBurger broMadeBurger = getBroMadeBurger();
+        broMadeBurger.cook();
+    }
+
+    public static HomeMadeBurger getMomMadeBurger() {
+        return new HomeMadeBurger() {
+            @Override
+            public void cook() {
+                System.out.println("집에서 만드는 엄마표 수제버거");
+                System.out.println("재료 : 빵, 소고기패티, 해시브라운, 양상추, 마요네즈");
+            }
+        };
+    }
+
+    private static HomeMadeBurger getBroMadeBurger() {
+        return new HomeMadeBurger() {
+            @Override
+            public void cook() {
+                System.out.println("집에서 만드는 아빠표 군대리아");
+                System.out.println("재료 : 빵, 치킨패티, 양배추 샐러드, 딸기잼, 치즈, 삶은 계란");
+            }
+        };
+    }
+}
+
+abstract class HomeMadeBurger {
+    public abstract void cook();
+}
