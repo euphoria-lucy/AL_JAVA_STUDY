2d933c2c7c0133785bc4ecb12b8898fca3de5e40
misung <voielactee7@naver.com>
2024-01-18 14:08:24 +0900

    [JAVA] Join

--- src/chap_12/clean/CleanRunnable.java
+++ src/chap_12/clean/CleanRunnable.java
@@ -7,6 +7,12 @@ public class CleanRunnable implements Runnable {
 
         for (int i = 2; i <= 10; i += 2) {
             System.out.println("(직원) " + i + " 번 방 청소 중 (Runnable)");
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
 
         System.out.println("---- 직원 청소 끝 (Runnable) ----");
