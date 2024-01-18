f5a22e3b283fc0be3feb790c6a1ad288942573b1
misung <voielactee7@naver.com>
2024-01-18 19:00:44 +0900

    [JAVA] 출력 - 실수

--- src/chap_13/_02_Output.java
+++ src/chap_13/_02_Output.java
@@ -17,5 +17,13 @@ public class _02_Output {
         System.out.printf("%,17d%n", 700000000); // ________700,000,000 (세자리마다 콤마로 구분)
 
         System.out.printf("%-7d%n", 205); // 205____ (7자리의 공간을 확보하고 나서 왼쪽 정렬 205 출력)
+
+        System.out.println("------- 실수 -------");
+        System.out.printf("%f%n", Math.PI); // 3.141593 (일반 출력)
+        System.out.printf("%.2f%n", Math.PI); // 3.14 (소수점 둘째자리)
+        System.out.printf("%7.2f%n", Math.PI); // ___3.14 (7자리 공간 확보하고 소수점 둘째자리)
+        System.out.printf("%-7.2f%n", Math.PI); // 3.14___ (왼쪽 정렬)
+        System.out.printf("%07.2f%n", Math.PI); // 0003.14 (빈 공간을 0으로 채우기)
+        System.out.printf("%+7.2f%n", Math.PI); // __+3.14 (항상 +- 기호 표시)
     }
 }
